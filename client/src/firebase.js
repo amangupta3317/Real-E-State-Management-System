fb9b5190823f0dc0e95222ad11d14914c42d0365
amangupta3317 <amangupta3317@gmail.com>
2024-05-11 11:38:53 +0530

    new project

---
+++ client/src/firebase.js
@@ -0,0 +1,61 @@
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
+
+// // Your web app's Firebase configuration
+// const firebaseConfig = {
+//   apiKey: "AIzaSyDaOLFv9GfJyXYeRMRHAPcTH6erjDPAGCU",
+//   authDomain: "mern-estate.firebaseapp.com",
+//   projectId: "mern-estate",
+//   storageBucket: "mern-estate.appspot.com",
+//   messagingSenderId: "1078482850952",
+//   appId: "1:1078482850952:web:28f19139ab77246602fb3d",
+// };
+
+// // Initialize Firebase
+// export const app = initializeApp(firebaseConfig);
+
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
+
+// // Your web app's Firebase configuration
+// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// const firebaseConfig = {
+//   apiKey: "AIzaSyDaOLFv9GfJyXYeRMRHAPcTH6erjDPAGCU",
+//   authDomain: "estate-1d632.firebaseapp.com",
+//   projectId: "estate-1d632",
+//   storageBucket: "estate-1d632.appspot.com",
+//   messagingSenderId: "1002487983277",
+//   appId: "1:1002487983277:web:2c2303e71de1bd28566a24",
+//   measurementId: "G-0NPHDQBC78",
+// };
+
+// // Initialize Firebase
+// export const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDh2OjR-LlrZw29YihHq5fCj59FzA4Wnd0",
+  authDomain: "aman-4c2bd.firebaseapp.com",
+  projectId: "aman-4c2bd",
+  storageBucket: "aman-4c2bd.appspot.com",
+  messagingSenderId: "58659242040",
+  appId: "1:58659242040:web:c8b9ccc8be281404b6e6cd",
+  measurementId: "G-DP93JGYL7Q",
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
