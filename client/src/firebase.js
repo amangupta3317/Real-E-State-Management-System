e8b7330d9842b4e1fff48c7d03ef5297812defe8
amangupta3317 <amangupta3317@gmail.com>
2024-05-11 12:11:42 +0530

    api key commit

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -1,43 +1,3 @@
-// // Import the functions you need from the SDKs you need
-// import { initializeApp } from "firebase/app";
-// // TODO: Add SDKs for Firebase products that you want to use
-// // https://firebase.google.com/docs/web/setup#available-libraries
-
-// // Your web app's Firebase configuration
-// const firebaseConfig = {
-//   apiKey: "AIzaSyDaOLFv9GfJyXYeRMRHAPcTH6erjDPAGCU",
-//   authDomain: "mern-estate.firebaseapp.com",
-//   projectId: "mern-estate",
-//   storageBucket: "mern-estate.appspot.com",
-//   messagingSenderId: "1078482850952",
-//   appId: "1:1078482850952:web:28f19139ab77246602fb3d",
-// };
-
-// // Initialize Firebase
-// export const app = initializeApp(firebaseConfig);
-
-// // Import the functions you need from the SDKs you need
-// import { initializeApp } from "firebase/app";
-// import { getAnalytics } from "firebase/analytics";
-// // TODO: Add SDKs for Firebase products that you want to use
-// // https://firebase.google.com/docs/web/setup#available-libraries
-
-// // Your web app's Firebase configuration
-// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-// const firebaseConfig = {
-//   apiKey: "AIzaSyDaOLFv9GfJyXYeRMRHAPcTH6erjDPAGCU",
-//   authDomain: "estate-1d632.firebaseapp.com",
-//   projectId: "estate-1d632",
-//   storageBucket: "estate-1d632.appspot.com",
-//   messagingSenderId: "1002487983277",
-//   appId: "1:1002487983277:web:2c2303e71de1bd28566a24",
-//   measurementId: "G-0NPHDQBC78",
-// };
-
-// // Initialize Firebase
-// export const app = initializeApp(firebaseConfig);
-// const analytics = getAnalytics(app);
-
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
@@ -47,7 +7,7 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDh2OjR-LlrZw29YihHq5fCj59FzA4Wnd0",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: "aman-4c2bd.firebaseapp.com",
   projectId: "aman-4c2bd",
   storageBucket: "aman-4c2bd.appspot.com",
